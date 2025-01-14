69941c95fb6cc324841bb7961cf413978d01eaa8
Mario V. <83434137+mariov-computer@users.noreply.github.com>
2025-01-13 18:23:05 -0800

    Update CreditCardMask.java

--- CreditCardMask.java
+++ CreditCardMask.java
@@ -1,4 +1,8 @@
-// Challenge#1
+// Challenge#1 Prompt:
+// Usually when you buy something, you're asked whether your credit card number, phone number or answer to your most secret question is still correct. 
+//However, since someone could look over your shoulder, you don't want that shown on your screen. Instead, we mask it.
+//Your task is to write a function maskify, which changes all but the last four characters into '#'.
+//Begin CreditCardMask.java
 public class Maskify {
     public static String maskify(String str) {
         if (str == null || str.length() <= 4) {
