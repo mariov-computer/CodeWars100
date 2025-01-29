e2920b4df2b49df55f979bfcae480cd9c8c56e03
Mario V. <83434137+mariov-computer@users.noreply.github.com>
2025-01-28 16:28:56 -0800

    Update PlayBanjo.java

--- PlayBanjo.java
+++ PlayBanjo.java
@@ -7,6 +7,13 @@
 //Begin PlayBanjo.java [Java]
 public class Banjo{
   public static String areYouPlayingBanjo(String name){
+if (name.startsWith("R") || name.startsWith("r")) {
+            return name + " plays banjo";
+        } else {
+            return name + " does not play banjo";
+        }
+    }
+
   }
 }
 
